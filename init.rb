15b26e540af7917fec13a3733883eaa8b16af3e5
vagner.jakub <jakub.vagner@active24.cz>
2020-10-17 17:40:53 +0200

    description ..

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_plugin_project_author do
-  name 'Redmine Plugin Project Author plugin'
-  author 'Author name'
+  name 'add author for projects'
+  author 'Jakub Vagner'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/vagnerj/redmine-plugin-project-author'
+  author_url 'https://snekovo.info/'
 end
