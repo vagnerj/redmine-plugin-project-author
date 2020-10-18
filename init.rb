a4c5011e5ffb7da4213ee01f674786125fe93c87
vagner.jakub <jakub.vagner@active24.cz>
2020-10-18 03:43:59 +0200

    no suitable hook and no chain_filter_methods, so using filters .. missing view wIP

--- init.rb
+++ init.rb
@@ -1,7 +1,24 @@
+require 'redmine'
+
+ActiveSupport::Reloader.to_prepare do
+
+  require 'redmine_plugin_project_author/project_model_patch'
+
+  require_dependency 'project'
+  # Guards against including the module multiple time (like in tests)
+  # and registering multiple callbacks
+  unless Project.included_modules.include? RedminePluginProjectAuthor::ProjectPatch
+    Project.send(:include, RedminePluginProjectAuthor::ProjectPatch)
+  end
+
+end
+
+require 'redmine_plugin_project_author/project_author_view_hook'
+
 Redmine::Plugin.register :redmine_plugin_project_author do
-  name 'add author for projects'
+  name 'Project with Author field'
   author 'Jakub Vagner'
-  description 'This is a plugin for Redmine'
+  description 'This is a plugin for Redmine which adds author field to project configuration'
   version '0.0.1'
   url 'https://github.com/vagnerj/redmine-plugin-project-author'
   author_url 'https://snekovo.info/'
